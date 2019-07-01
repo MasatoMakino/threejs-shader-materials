05ae721a8f792f71b0b774f3e1ab8b0f6ce30413
MasatoMakino <unagiinu.g@gmail.com>
2019-07-01 21:55:42 +0900

    add : contour material.

---
+++ demoSrc/demoContour.js
@@ -0,0 +1,52 @@
+import {
+  Mesh,
+  Fog,
+  PointLight,
+  PointLightHelper,
+  TorusGeometry,
+  Color
+} from "three";
+import { Common } from "./Common";
+import { ContourMaterial } from "../bin/contour/ContourMaterial";
+
+export class StudyContourMap {
+  constructor() {
+    const W = 640;
+    const H = 480;
+
+    const scene = Common.initScene();
+    scene.fog = new Fog(0x000000, 80, 160);
+    Common.initLight(scene);
+    const camera = Common.initCamera(scene, W, H);
+    const renderer = Common.initRenderer(W, H);
+    const control = Common.initControl(camera, renderer);
+    Common.initHelper(scene);
+    this.initObject(scene);
+    Common.render(control, renderer, scene, camera);
+  }
+
+  initObject(scene) {
+    const spot = new PointLight(0xffffff, 3, 0, 2);
+    spot.position.set(10, 20, 30);
+    scene.add(spot);
+    const helper = new PointLightHelper(spot);
+    scene.add(helper);
+
+    const geo = new TorusGeometry(10, 4, 32, 32);
+
+    const mat = new ContourMaterial({
+      opacity: 0.75,
+      fog: scene.fog !== undefined
+    });
+    mat.loadMap("./textures/contour_glow.png", geo);
+    mat.startGlow();
+    mat.emissive = new Color(0xffff00);
+
+    const mesh = new Mesh(geo, mat);
+    scene.add(mesh);
+  }
+}
+
+window.onload = () => {
+  const study = new StudyContourMap();
+};
---
+++ src/contour/ContourMaterial.ts
@@ -0,0 +1,75 @@
+/**
+ * テクスチャを等高線状にマップするマテリアル。
+ * マッピング以外の昨日はMeshPhongMaterialに準じる。
+ */
+
+import {
+  UniformsUtils,
+  ShaderMaterialParameters,
+  Texture,
+  TextureLoader,
+  DoubleSide,
+  Geometry,
+  BufferGeometry
+} from "three";
+import { ShaderPhongMaterial } from "../ShaderPhongMaterial";
+
+import FragmentShader from "./ContourMaterial.frag.glsl";
+import VertexShader from "../ShaderPhongMaterial.vert.glsl";
+
+export class ContourMaterial extends ShaderPhongMaterial {
+  get map(): Texture {
+    return this._map;
+  }
+  public loadMap(url: string, geo: Geometry | BufferGeometry) {
+    this._map = new TextureLoader().load(url, texture => {
+      if (this.uniforms && this.uniforms.map) {
+        this.uniforms.map.value = texture;
+      }
+    });
+
+    geo.computeBoundingBox();
+    this.uniforms.top.value = geo.boundingBox.max.y;
+    this.uniforms.bottom.value = geo.boundingBox.min.y;
+  }
+  private _map: Texture;
+
+  constructor(parameters?: ShaderMaterialParameters) {
+    super(VertexShader, FragmentShader, parameters);
+  }
+
+  protected initDefines(): void {
+    super.initDefines();
+    this.defines.USE_MESH_POSITION = true;
+  }
+
+  protected initUniforms(): void {
+    this.uniforms = UniformsUtils.merge([
+      ShaderPhongMaterial.getBasicUniforms(),
+      {
+        top: { value: 1.0 },
+        bottom: { value: -1.0 }
+      }
+    ]);
+  }
+
+  protected initDefaultSetting(parameters?: ShaderMaterialParameters): void {
+    super.initDefaultSetting(parameters);
+
+    if (parameters.transparent == null) {
+      this.transparent = true;
+    } else {
+      this.transparent = parameters.transparent;
+    }
+
+    if (this.transparent && parameters.alphaTest == null) {
+      this.alphaTest = 0.5;
+    }
+
+    if (parameters.side == null) {
+      this.side = DoubleSide;
+    } else {
+      this.side = parameters.side;
+    }
+  }
+}
