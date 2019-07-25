6227d5ccecc5146aa2622556769549b40841d4af
MasatoMakino <unagiinu.g@gmail.com>
2019-07-25 14:54:31 +0900

    update : bin and docs.

--- bin/skyCloud/SkyCloudMaterial.frag.glsl.js
+++ bin/skyCloud/SkyCloudMaterial.frag.glsl.js
@@ -42,16 +42,6 @@ uniform float cloudBottomSaturation;
 #include <logdepthbuf_pars_fragment>
 #include <clipping_planes_pars_fragment>
 
-  
-/**
- * Author @patriciogv - 2015
- * http://patriciogonzalezvivo.com
- */
-float random (in vec2 _st) {
-    return fract(sin(dot(_st.xy,
-                         vec2(12.9898,78.233)))*
-        43758.5453123);
-}
 
 /**
  * Based on Morgan McGuire @morgan3d
@@ -62,10 +52,10 @@ float noise (in vec2 _st) {
     vec2 f = fract(_st);
 
     // Four corners in 2D of a tile
-    float a = random(i);
-    float b = random(i + vec2(1.0, 0.0));
-    float c = random(i + vec2(0.0, 1.0));
-    float d = random(i + vec2(1.0, 1.0));
+    float a = rand(i);
+    float b = rand(i + vec2(1.0, 0.0));
+    float c = rand(i + vec2(0.0, 1.0));
+    float d = rand(i + vec2(1.0, 1.0));
 
     vec2 u = f * f * (3.0 - 2.0 * f);
 
