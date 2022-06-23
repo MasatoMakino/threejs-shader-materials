90de62278a299845ef7b48da48d4c605d7e05adc
MasatoMakino <makino.masato.g@gmail.com>
2022-06-23 15:12:14 +0900

    refactoring

--- demoSrc/demoRimEdges.js
+++ demoSrc/demoRimEdges.js
@@ -1,59 +1,65 @@
 import { Common } from "./Common";
-import { BoxGeometry, Color, Fog, LineSegments } from "three";
+import {BoxBufferGeometry, BoxGeometry, Color, Fog, LineSegments, SphereBufferGeometry, SphereGeometry} from "three";
+import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils";
 import { RAFTicker, RAFTickerEventType } from "@masatomakino/raf-ticker";
 import { RimEdgesMaterial, RimEdgesGeometry } from "..";
 import GUI from "lil-gui";
 import { CommonGUI } from "./CommonGUI";
 
 export class Study {
-    constructor() {
-        const W = 640;
-        const H = 480;
+  constructor() {
+    const W = 640;
+    const H = 480;
 
-        const scene = Common.initScene();
-        scene.fog = new Fog(0x000000, 80, 160);
-        Common.initLight(scene);
+    const scene = Common.initScene();
+    scene.fog = new Fog(0x000000, 80, 160);
+    Common.initLight(scene);
 
-        const camera = Common.initCamera(scene, W, H);
-        const renderer = Common.initRenderer(W, H);
-        Common.initControl(camera, renderer);
-        Common.initHelper(scene);
+    const camera = Common.initCamera(scene, W, H);
+    const renderer = Common.initRenderer(W, H);
+    Common.initControl(camera, renderer);
+    Common.initHelper(scene);
 
-        const mat = this.initObject(scene);
-        RAFTicker.on(RAFTickerEventType.tick, () => {
-            renderer.render(scene, camera);
-        });
-        this.initGUI(mat);
-    }
+    const mat = this.initObject(scene);
+    RAFTicker.on(RAFTickerEventType.tick, () => {
+      renderer.render(scene, camera);
+    });
+    this.initGUI(mat);
+  }
 
-    initObject(scene) {
-        const edge = new RimEdgesGeometry(new BoxGeometry(20, 20, 20), 7);
-        const mat = new RimEdgesMaterial({
-            fog: scene.fog !== undefined,
-        });
+  initObject(scene) {
+    const box = new BoxBufferGeometry(20, 20, 20);
+    box.translate(-30, 0, 0);
+    const sphere = new SphereBufferGeometry(20);
 
-        mat.color = new Color(0xffffff);
-        mat.uniformOpacity = 1.0;
-        mat.transparent = true;
+    const geo = mergeBufferGeometries([box, sphere]);
+    const edge = new RimEdgesGeometry(geo);
+    const mat = new RimEdgesMaterial({
+      fog: scene.fog !== undefined,
+    });
 
-        const seg = new LineSegments(edge, mat);
-        scene.add(seg);
-        seg.computeLineDistances();
+    mat.color = new Color(0xffffff);
+    mat.uniformOpacity = 1.0;
+    mat.transparent = true;
 
-        return mat;
-    }
+    const seg = new LineSegments(edge, mat);
+    scene.add(seg);
+    seg.computeLineDistances();
 
-    initGUI(mat) {
-        const gui = new GUI();
-        CommonGUI.initBasicMaterialGUI(gui, mat);
+    return mat;
+  }
 
-        const folder = gui.addFolder("LineDashed");
-        folder.add(mat, "scale", 1, 10, 0.1);
-        folder.add(mat, "dashSize", 1, 10);
-        folder.add(mat, "totalSize", 1, 10);
-    }
+  initGUI(mat) {
+    const gui = new GUI();
+    CommonGUI.initBasicMaterialGUI(gui, mat);
+
+    const folder = gui.addFolder("LineDashed");
+    folder.add(mat, "scale", 1, 10, 0.1);
+    folder.add(mat, "dashSize", 1, 10);
+    folder.add(mat, "totalSize", 1, 10);
+  }
 }
 
 window.onload = () => {
-    new Study();
+  new Study();
 };
--- src/rimEdges/RimEdgesGeometry.ts
+++ src/rimEdges/RimEdgesGeometry.ts
@@ -1,6 +1,8 @@
 import {
+  BufferAttribute,
   BufferGeometry,
   Float32BufferAttribute,
+  InterleavedBufferAttribute,
   MathUtils,
   Triangle,
   Vector3,
@@ -32,35 +34,121 @@ export class RimEdgesGeometry extends BufferGeometry {
     const direction: number[] = [];
     const collapse: number[] = [];
 
-    const thresholdDot = Math.cos(MathUtils.DEG2RAD * thresholdAngle);
-
-    const edgeData: Map<string, EdgeData> = new Map<string, EdgeData>();
-
-    let key;
-    const keys = ["a", "b", "c"];
-
     // prepare source geometry
+    const geometry2 = RimEdgesGeometry.margeGeometry(geometry);
+
+    const indexedPositions = RimEdgesGeometry.convertAttributeToVector3Array(
+      geometry2.attributes.position
+    );
+    const faceNormalInfos: FaceNormalInfo[] =
+      RimEdgesGeometry.getFaceNormalInfo(geometry2);
+    const edgeData: Map<string, EdgeData> =
+      RimEdgesGeometry.extractEdgeDataMap(faceNormalInfos);
+
+    // generate vertices
+    const v3 = new Vector3();
+    const n = new Vector3();
+    const n1 = new Vector3();
+    const n2 = new Vector3();
+    const d = new Vector3();
+
+    const thresholdDot = Math.cos(MathUtils.DEG2RAD * thresholdAngle);
+    for (let key in edgeData) {
+      const edge = edgeData[key];
+
+      if (edge.face2 == null) {
+        continue;
+      }
+
+      // an edge is only rendered if the angle (in degrees) between the face normals of the adjoining faces exceeds this value. default = 1 degree.
+      if (
+        faceNormalInfos[edge.face1].normal.dot(
+          faceNormalInfos[edge.face2].normal
+        ) <= thresholdDot
+      ) {
+        const vertex1 = indexedPositions[edge.index1];
+        const vertex2 = indexedPositions[edge.index2];
+
+        vertices.push(vertex1.x, vertex1.y, vertex1.z);
+        vertices.push(vertex2.x, vertex2.y, vertex2.z);
+
+        collapse.push(0, 1);
+
+        d.subVectors(vertex2, vertex1);
+        n.copy(d).normalize();
+        direction.push(d.x, d.y, d.z);
+
+        n1.copy(faceNormalInfos[edge.face1].normal);
+        n1.crossVectors(n, n1);
+
+        d.subVectors(vertex1, vertex2);
+        n.copy(d).normalize();
+        direction.push(d.x, d.y, d.z);
+
+        n2.copy(faceNormalInfos[edge.face2]?.normal ?? new Vector3(0, 0, 0));
+        n2.crossVectors(n, n2);
+
+        v3.copy(vertex1).add(n1); // control0
+        control0.push(v3.x, v3.y, v3.z);
+        v3.copy(vertex1).add(n2); // control1
+        control1.push(v3.x, v3.y, v3.z);
+
+        v3.copy(vertex2).add(n1); // control0
+        control0.push(v3.x, v3.y, v3.z);
+        v3.copy(vertex2).add(n2); // control1
+        control1.push(v3.x, v3.y, v3.z);
+      }
+    }
+
+    // build geometry
+    this.setAttribute("position", new Float32BufferAttribute(vertices, 3));
+    this.setAttribute("control0", new Float32BufferAttribute(control0, 3));
+    this.setAttribute("control1", new Float32BufferAttribute(control1, 3));
+    this.setAttribute("direction", new Float32BufferAttribute(direction, 3));
+    this.setAttribute("collapse", new Float32BufferAttribute(collapse, 1));
+  }
+
+  private static margeGeometry(geometry: BufferGeometry): BufferGeometry {
     const geometry2a = geometry.clone();
     const ratio =
       geometry2a.attributes.position.array.length / geometry2a.index.count;
     const geometry2 = mergeVertices(geometry2a, ratio);
     geometry2.computeVertexNormals();
+    return geometry2;
+  }
 
-    const sourceVertices = geometry2.attributes.position;
-    const sv = [];
-
-    for (let s = 0; s < sourceVertices.array.length; s++) {
-      sv.push(
+  /**
+   *
+   * @param attribute
+   * @private
+   */
+  private static convertAttributeToVector3Array(
+    attribute: BufferAttribute | InterleavedBufferAttribute
+  ): Vector3[] {
+    const array = [];
+    for (let s = 0; s < attribute.array.length; s++) {
+      array.push(
         new Vector3(
-          sourceVertices.array[s * 3],
-          sourceVertices.array[s * 3 + 1],
-          sourceVertices.array[s * 3 + 2]
+          attribute.array[s * 3],
+          attribute.array[s * 3 + 1],
+          attribute.array[s * 3 + 2]
         )
       );
     }
+    return array;
+  }
 
-    const index = geometry2.index;
+  /**
+   * Get normals from BufferGeometry
+   *
+   * @param geometry
+   * @private
+   */
+  private static getFaceNormalInfo(geometry: BufferGeometry): FaceNormalInfo[] {
+    const index = geometry.index;
     const faceCount = index.count / 3;
+    const sourceVertices = geometry.attributes.position;
+
     const faceNormalInfos: FaceNormalInfo[] = [];
     const _triangle = new Triangle();
     const a = new Vector3();
@@ -85,23 +173,34 @@ export class RimEdgesGeometry extends BufferGeometry {
       faceNormalInfos.push(faceNormalInfo);
     }
 
+    return faceNormalInfos;
+  }
+
+  private static extractEdgeDataMap(
+    faceNormalInfos: FaceNormalInfo[]
+  ): Map<string, EdgeData> {
+    const keys = ["a", "b", "c"];
+    const edgeData: Map<string, EdgeData> = new Map<string, EdgeData>();
+
     for (let i = 0; i < faceNormalInfos.length; i++) {
       const faceNormalInfo = faceNormalInfos[i];
 
-      for (let j = 0; j < 3; j++) {
+      for (let j = 0; j < keys.length; j++) {
         const edge1 = faceNormalInfo[keys[j]];
         const edge2 = faceNormalInfo[keys[(j + 1) % 3]];
         const edgeMin = Math.min(edge1, edge2);
         const edgeMax = Math.max(edge1, edge2);
 
-        key = edgeMin + "," + edgeMax;
+        const key = edgeMin + "," + edgeMax;
+        const hash = `${ edge1 }_${ edge2 }`;
+        const reverseHash = `${ edge2 }_${ edge1 }`;
 
         if (edgeData[key] === undefined) {
           edgeData[key] = {
             index1: edgeMin,
             index2: edgeMax,
             face1: i,
-            face2: undefined,
+            face2: null,
           };
         } else {
           edgeData[key].face2 = i;
@@ -109,60 +208,6 @@ export class RimEdgesGeometry extends BufferGeometry {
       }
     }
 
-    // generate vertices
-    const v3 = new Vector3();
-    const n = new Vector3();
-    const n1 = new Vector3();
-    const n2 = new Vector3();
-    const d = new Vector3();
-
-    for (key in edgeData) {
-      const edge = edgeData[key];
-
-      // an edge is only rendered if the angle (in degrees) between the face normals of the adjoining faces exceeds this value. default = 1 degree.
-      if (
-        edge.face2 === undefined ||
-        faceNormalInfos[edge.face1].normal.dot(
-          faceNormalInfos[edge.face2].normal
-        ) <= thresholdDot
-      ) {
-        const vertex1 = sv[edge.index1];
-        const vertex2 = sv[edge.index2];
-
-        vertices.push(vertex1.x, vertex1.y, vertex1.z);
-        vertices.push(vertex2.x, vertex2.y, vertex2.z);
-
-        collapse.push(0, 1);
-
-        d.subVectors(vertex2, vertex1);
-        n.copy(d).normalize();
-        direction.push(d.x, d.y, d.z);
-        n1.copy(faceNormalInfos[edge.face1].normal);
-        n1.crossVectors(n, n1);
-
-        d.subVectors(vertex1, vertex2);
-        n.copy(d).normalize();
-        n2.copy(faceNormalInfos[edge.face2].normal);
-        n2.crossVectors(n, n2);
-        direction.push(d.x, d.y, d.z);
-
-        v3.copy(vertex1).add(n1); // control0
-        control0.push(v3.x, v3.y, v3.z);
-        v3.copy(vertex1).add(n2); // control1
-        control1.push(v3.x, v3.y, v3.z);
-
-        v3.copy(vertex2).add(n1); // control0
-        control0.push(v3.x, v3.y, v3.z);
-        v3.copy(vertex2).add(n2); // control1
-        control1.push(v3.x, v3.y, v3.z);
-      }
-    }
-
-    // build geometry
-    this.setAttribute("position", new Float32BufferAttribute(vertices, 3));
-    this.setAttribute("control0", new Float32BufferAttribute(control0, 3));
-    this.setAttribute("control1", new Float32BufferAttribute(control1, 3));
-    this.setAttribute("direction", new Float32BufferAttribute(direction, 3));
-    this.setAttribute("collapse", new Float32BufferAttribute(collapse, 1));
+    return edgeData;
   }
 }
