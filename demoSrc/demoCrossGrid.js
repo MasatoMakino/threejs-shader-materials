6cae99e9af15eb33f036bb5d6f53fe25a0158037
MasatoMakino <makino.masato.g@gmail.com>
2023-12-27 13:59:11 +0900

    fix : cross grig material

--- demoSrc/demoCrossGrid.js
+++ demoSrc/demoCrossGrid.js
@@ -32,7 +32,7 @@ export class StudyCrossGrid {
   }
 
   initObject(scene) {
-    const spot = new PointLight(0xffffff, 1, 0, 2);
+    const spot = new PointLight(0xffffff, 7_000);
     spot.position.set(10, 20, 30);
     scene.add(spot);
     const helper = new PointLightHelper(spot);
