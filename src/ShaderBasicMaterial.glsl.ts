c8a937554bedf4563320cf2f4d85a4367d620211
MasatoMakino <makino.masato.g@gmail.com>
2023-12-28 17:38:44 +0900

    fix : rim basic material

--- src/ShaderBasicMaterial.glsl.ts
+++ src/ShaderBasicMaterial.glsl.ts
@@ -12,8 +12,8 @@ varying vec3 vViewPosition;
 varying vec2 uvPosition;
 
 #include <common>
+#include <batching_pars_vertex>
 #include <uv_pars_vertex>
-#include <uv2_pars_vertex>
 #include <envmap_pars_vertex>
 #include <color_pars_vertex>
 #include <fog_pars_vertex>
@@ -21,20 +21,26 @@ varying vec2 uvPosition;
 #include <skinning_pars_vertex>
 #include <logdepthbuf_pars_vertex>
 #include <clipping_planes_pars_vertex>
+
 void main() {
+
 	#include <uv_vertex>
-	#include <uv2_vertex>
-    uvPosition = uv;
+	uvPosition = uv;
 	
     #include <color_vertex>
 	#include <morphcolor_vertex>
+	#include <batching_vertex>
+
 	#if defined ( USE_ENVMAP ) || defined ( USE_SKINNING )
+
 		#include <beginnormal_vertex>
 		#include <morphnormal_vertex>
 		#include <skinbase_vertex>
 		#include <skinnormal_vertex>
 		#include <defaultnormal_vertex>
+
 	#endif
+	
 	#include <begin_vertex>
 	#include <morphtarget_vertex>
 	#include <skinning_vertex>
@@ -51,58 +57,79 @@ void main() {
 	#include <worldpos_vertex>
 	#include <envmap_vertex>
 	#include <fog_vertex>
+	
 }`;
 
 //language=glsl
 export const fragment = /* GLSL */ `
 uniform vec3 diffuse;
 uniform float opacity;
+
 #ifndef FLAT_SHADED
-	varying vec3 vNormal;
+
+    varying vec3 vNormal;
+
 #endif
+
 #include <common>
 #include <dithering_pars_fragment>
 #include <color_pars_fragment>
 #include <uv_pars_fragment>
-#include <uv2_pars_fragment>
 #include <map_pars_fragment>
 #include <alphamap_pars_fragment>
 #include <alphatest_pars_fragment>
+#include <alphahash_pars_fragment>
 #include <aomap_pars_fragment>
 #include <lightmap_pars_fragment>
 #include <envmap_common_pars_fragment>
 #include <envmap_pars_fragment>
-#include <cube_uv_reflection_fragment>
 #include <fog_pars_fragment>
 #include <specularmap_pars_fragment>
 #include <logdepthbuf_pars_fragment>
 #include <clipping_planes_pars_fragment>
+
 void main() {
+
 	#include <clipping_planes_fragment>
+	
 	vec4 diffuseColor = vec4( diffuse, opacity );
+	
 	#include <logdepthbuf_fragment>
 	#include <map_fragment>
 	#include <color_fragment>
 	#include <alphamap_fragment>
 	#include <alphatest_fragment>
+	#include <alphahash_fragment>
 	#include <specularmap_fragment>
+	
 	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+
 	// accumulation (baked indirect lighting only)
 	#ifdef USE_LIGHTMAP
-		vec4 lightMapTexel = texture2D( lightMap, vUv2 );
+
+		vec4 lightMapTexel = texture2D( lightMap, vLightMapUv );
 		reflectedLight.indirectDiffuse += lightMapTexel.rgb * lightMapIntensity * RECIPROCAL_PI;
+
 	#else
+
 		reflectedLight.indirectDiffuse += vec3( 1.0 );
+
 	#endif
+
 	// modulation
 	#include <aomap_fragment>
+
 	reflectedLight.indirectDiffuse *= diffuseColor.rgb;
+	
 	vec3 outgoingLight = reflectedLight.indirectDiffuse;
+	
 	#include <envmap_fragment>
-	#include <output_fragment>
+
+	#include <opaque_fragment>
 	#include <tonemapping_fragment>
-	#include <encodings_fragment>
+	#include <colorspace_fragment>
 	#include <fog_fragment>
 	#include <premultiplied_alpha_fragment>
 	#include <dithering_fragment>
+	
 }`;
