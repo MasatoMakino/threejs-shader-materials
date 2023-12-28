4331f086c178199f85664d8c56c44570b082967a
MasatoMakino <makino.masato.g@gmail.com>
2023-12-28 17:22:50 +0900

    fix : hex grid material

--- src/hexGrid/HexGridMaterial.glsl.ts
+++ src/hexGrid/HexGridMaterial.glsl.ts
@@ -26,15 +26,15 @@ uniform float gridWeight;
 #include <dithering_pars_fragment>
 #include <color_pars_fragment>
 #include <uv_pars_fragment>
-#include <uv2_pars_fragment>
 #include <map_pars_fragment>
+#include <alphamap_pars_fragment>
 #include <alphatest_pars_fragment>
+#include <alphahash_pars_fragment>
 #include <aomap_pars_fragment>
 #include <lightmap_pars_fragment>
 #include <emissivemap_pars_fragment>
 #include <envmap_common_pars_fragment>
 #include <envmap_pars_fragment>
-#include <cube_uv_reflection_fragment>
 #include <fog_pars_fragment>
 #include <bsdfs>
 #include <lights_pars_begin>
@@ -46,6 +46,7 @@ uniform float gridWeight;
 #include <specularmap_pars_fragment>
 #include <logdepthbuf_pars_fragment>
 #include <clipping_planes_pars_fragment>
+
 void main() {
     #include <clipping_planes_fragment>
   
@@ -78,23 +79,29 @@ void main() {
 
     // #include <alphamap_fragment>
     #include <alphatest_fragment>
+    #include <alphahash_fragment>
     #include <specularmap_fragment>
     #include <normal_fragment_begin>
     #include <normal_fragment_maps>
     #include <emissivemap_fragment>
+
     // accumulation
     #include <lights_phong_fragment>
     #include <lights_fragment_begin>
     #include <lights_fragment_maps>
     #include <lights_fragment_end>
+
     // modulation
     #include <aomap_fragment>
+
     vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+
     #include <envmap_fragment>
-    #include <output_fragment>
+    #include <opaque_fragment>
     #include <tonemapping_fragment>
-    #include <encodings_fragment>
+    #include <colorspace_fragment>
     #include <fog_fragment>
     #include <premultiplied_alpha_fragment>
     #include <dithering_fragment>
+
 }`;
