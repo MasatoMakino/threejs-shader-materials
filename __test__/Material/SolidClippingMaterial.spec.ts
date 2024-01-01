569b1b9511a3c3be8bdd202cc7008bf0c5ba65c1
MasatoMakino <makino.masato.g@gmail.com>
2024-01-01 17:40:44 +0900

    add : SolidClippingMaterial unit tests and fix cutSectionColor type

---
+++ __test__/Material/SolidClippingMaterial.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { Color, Mesh, PlaneGeometry } from "three";
+import { SolidClippingMaterial } from "../../src/index.js";
+import { initScene, testRendering } from "../Common.js";
+
+describe("SolidClippingMaterial", () => {
+  let material: SolidClippingMaterial;
+  beforeEach(() => {
+    material = new SolidClippingMaterial();
+  });
+
+  it("should be able to create a material instance", () => {
+    const material = new SolidClippingMaterial();
+    expect(material).toBeInstanceOf(SolidClippingMaterial);
+    expect(material).toBeTruthy();
+  });
+
+  it("should handle glsl inclusion and render correctly", () => {
+    testRendering(material);
+  });
+
+  it("should get and set cutSectionColor correctly", () => {
+    const value = new Color(0x123456);
+    material.cutSectionColor = value;
+    expect(material.cutSectionColor).toEqual(value);
+  });
+});
